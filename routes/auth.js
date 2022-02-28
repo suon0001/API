8fbdd4cab7c0b88fcc10ddb989ec36e21671fe2a
suon0001 <suon0001@users.noreply.github.com>
2022-02-28 14:49:55 +0100

    Authentication part 2

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const user = require('../models/user');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 const { registerValidation } = require('../validation');
 
 //registration
@@ -13,13 +15,29 @@ if (error) {
 }
 
 //check if the email is already registered 
+const emailExit = await User.findOne({ email: req.body.email});
+if (emailExit) {
+    return res.status(400).json({ error: "Email already exists"});
+}
 
 
 //hash the password
-
+const salt = await bcrypt.genSalt(10);
+const password = await bcrypt.hash(req.body.password, salt);
 
 //create a user object and save in the database
+const userObject = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password
+})
 
+try {
+    const savedUser = await userObject.save();
+    res.json({ error: null, data: savedUser._id});
+} catch (error) {
+    return res.status(400).json({ error });
+}
  
 });
 
